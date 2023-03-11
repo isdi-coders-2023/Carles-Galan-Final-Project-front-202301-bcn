28fc1fa3154c468ad17781779902c30cd92a9d34
Carles Galan <galan.carles@gmail.com>
2023-03-12 00:02:05 +0100

    Refactor and add sucess toast

--- src/components/Modals/toasts.ts
+++ src/components/Modals/toasts.ts
@@ -1,17 +1,21 @@
 import "react-toastify/dist/ReactToastify.css";
-import { toast } from "react-toastify";
+import { toast, ToastOptions } from "react-toastify";
 
-const showErrorMessage = (message: string) => {
-  toast.error(message, {
-    position: "top-center",
-    autoClose: 3000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-    theme: "colored",
-  });
+const toasSetting: ToastOptions<{}> = {
+  position: "top-center",
+  autoClose: 3000,
+  hideProgressBar: false,
+  closeOnClick: true,
+  pauseOnHover: true,
+  draggable: true,
+  progress: undefined,
+  theme: "colored",
 };
 
-export default showErrorMessage;
+export const showErrorMessage = (message: string) => {
+  toast.error(message, toasSetting);
+};
+
+export const showSuccesMessage = (message: string) => {
+  toast.success(message, toasSetting);
+};
